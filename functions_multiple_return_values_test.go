8346d2ab4bd6f6e083fde412971ca4c91079c703
qqlongzhang <qqlongzhang@tencent.com>
2020-11-28 17:46:26 +0800

    remove dup

--- functions_multiple_return_values_test.go
+++ functions_multiple_return_values_test.go
@@ -10,20 +10,6 @@ import (
 	"testing"
 )
 
-func Visit(links []string, n *html.Node) []string {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				links = append(links, a.Val)
-			}
-		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = Visit(links, c)
-	}
-	return links
-}
-
 func findLinks(url string) ([]string, error) {
 	reply, err := http.Get(url)
 	if err != nil {
