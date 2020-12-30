59f741c44ec4ff2b5ddac584845fe473ca5c5d59
qqlongzhang <qqlongzhang@tencent.com>
2020-12-30 09:16:53 +0800

    forward

---
+++ ch1/e1.4.go
@@ -0,0 +1,38 @@
+// Dup2 prints the count and text of lines that appear more than once
+// in the input. It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	files := os.Args[1:]
+	if len(files) == 0 {
+		counts := make(map[string]int)
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			counts := make(map[string]int)
+			countLines(f, counts)
+			if len(counts) > 0 {
+				fmt.Println(arg)
+			}
+			f.Close()
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
